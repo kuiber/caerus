9cff606edb3070cc447f5559a4a2aff5d78b5245
Kavin Autar <kav@promed.com.au>
2018-10-17 22:54:39 +1100

    all working

--- caerus/timelapse.py
+++ caerus/timelapse.py
@@ -1,20 +1,22 @@
 from caerus.logger import logger
-from caerus.timelapse import TimeLapse, Camera
-from picamera import PiCamera
-from time import sleep
-from datetime import datetime
+from caerus.timelapse import TimeLapse
+import datetime
+from argparse import ArgumentParser
 
 logger.add_file_logger("/tmp/caerus.log")
 
 
 def main():
-    logger.debug("timelapse initialised")
-    camera = Camera()
+    parser = ArgumentParser()
+    parser.add_argument("project", help="project name where images are saved")
+    parser.add_argument("-i", "--interval", type=float, required=True, nargs=1, help="interval [float] in seconds between successive image captures")
+    parser.add_argument("-d", "--duration", type=float, required=True, nargs=1, help="duration [float] in seconds of the total timelapse duration")
+    args = parser.parse_args()
 
-    for i in range(0, 50):
-        timestamp = datetime.now().isoformat()
-        camera.snapshot(f"/home/pi/picture_{timestamp}.jpg")
-        sleep(3)
+    logger.debug(args)
+
+    timelapse = TimeLapse(args.project, args.interval[0], datetime.timedelta(seconds=args.duration[0]))
+    timelapse.start()
 
 
 if __name__ == "__main__":
--- caerus/timelapse/__init__.py
+++ caerus/timelapse/__init__.py
@@ -1,12 +1,35 @@
 from picamera import PiCamera
 from caerus.logger import logger
 from time import sleep
-from fractions import Fraction
+from os import path, makedirs
+import datetime
 
 
 class TimeLapse(object):
-    def __init__(self):
+    BASE_PROJECT_DIRECTORY = "/home/pi/timelapse"
+
+    def __init__(self, project_name: str, interval_s: float, duration: datetime.timedelta):
         logger.debug("initialising TimeLapse class")
+        self.project_name = project_name
+        self.interval = interval_s
+        self.duration = duration  # FIXME thread here
+        self.project_directory = path.join(TimeLapse.BASE_PROJECT_DIRECTORY, self.project_name)
+        if not path.exists(self.project_directory):
+            logger.info(f"making project directory '{self.project_directory}'")
+            makedirs(self.project_directory, exist_ok=True)
+
+    def start(self):
+        # FIXME check if file name exists - go to end of sequence - don't overwrite
+        logger.info(f"starting timelapse: taking a picture every {self.interval}s for {self.duration} interval")
+        end_time = datetime.datetime.now() + self.duration
+        counter = 0
+        while datetime.datetime.now() < end_time:
+            # filename = f"image_{datetime.datetime.now().isoformat()}.jpg"
+            filename = f"image_{counter:09}.jpg"
+            Camera.snapshot(path.join(self.project_directory, filename))
+            counter += 1
+            sleep(self.interval)
+        logger.info("timelapse complete")
 
 
 class Camera(object):
@@ -14,7 +37,7 @@ class Camera(object):
     __RESOLUTION = (3280, 2464)
 
     @staticmethod
-    def snapshot(file_destination):
+    def snapshot(file_destination: str):
         camera = PiCamera()
         camera.resolution = Camera.__RESOLUTION
         sleep(Camera.SLEEP_INTERVAL)
